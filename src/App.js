2c5f49a002b7d7c34eeb2141a6925b07a66046e0
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-01-09 17:49:42 +0300

    worked api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import Search from './components/search/search';
 
 function App() {
+
+  const handleOnSearchChange = (searchData) =>{
+    console.log(searchData);
+  }
+
   return (
-    <div className="App">
-     
-     
+    <div className="container">
+     <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		'X-RapidAPI-Key': 'e0ec75494fmsh1f3c37a197f84f5p1a758cjsn26a2e701536b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
