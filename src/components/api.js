78a327c0d9a8bf1a65132df8df5aacd6be6f6b0d
Feyza Karanfil Erat <56940562+feyzaerat@users.noreply.github.com>
2023-01-09 17:52:00 +0300

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: "GET",
 	headers: {
-		'X-RapidAPI-Key': 'e0ec75494fmsh1f3c37a197f84f5p1a758cjsn26a2e701536b',
+		'X-RapidAPI-Key': 'your api key is here',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	},
 };
